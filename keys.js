c71d40cf10044fa9c3e7aef558aba2246fb1eb65
Naomi Thomson <naomi.thomson@yahoo.com>
2017-05-19 17:11:48 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'rxFnd7w2StXgfnzL2qYfUhIV1',
+  consumer_secret: 'yn79ys4qNI4XZtcOYLQ8lSCf7Ohxf4tGh7Dfpkbs5I0h9aaLMV',
+  access_token_key: '808478049784066050-zSBoGft3bYVfoaEJAAuNaq3mq16EDT4',
+  access_token_secret: 'eU6umTNdty51x7BHLDiYVqhnEl1ui4tGvKzXgofWdfbls',
 }
